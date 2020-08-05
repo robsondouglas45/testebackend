b4b89e7e5351cf9e11a71dc3a94463d52a6c8177
Robson Douglas <robsonweb@hotmail.com>
2020-08-05 00:39:39 -0300

    Teste pronto

---
+++ Controllers/GithubController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using robsonteste.Models;
+
+namespace robsonteste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GithubController : ControllerBase
+    {       
+
+        IHttpClientFactory _clientFactory;
+
+        public GithubController(IHttpClientFactory clientFactory)
+        {          
+            _clientFactory = clientFactory;
+        }       
+
+        [HttpGet("users")]
+        public async Task<IActionResult> Usuario(string user = "dotnet")
+        {
+            var client = _clientFactory.CreateClient();
+            client.BaseAddress = new Uri("http://api.github.com/users/");
+            client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+            client.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+            var result = await client.GetStreamAsync(user);           
+            var response = JsonSerializer.DeserializeAsync<UserModel>(result);                      
+            return Ok(response);
+        }
+
+        [HttpGet("repositorys")]
+        public async Task<IActionResult> Repository(string user = "dotnet")
+        {
+            var client = _clientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://api.github.com/users/"); 
+            client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+            client.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+            var result = await client.GetStreamAsync(user + "/repos");          
+            return Ok(result);
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace robsonteste.Models
+{
+    public class UserModel
+    {
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+        [JsonPropertyName("login")]
+        public string login { get; set; }
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+        [JsonPropertyName("html_url")]
+        public string html_url { get; set; }
+        [JsonPropertyName("avatar_url")]
+        public string avatar_url { get; set; }
+
+    }
+}
